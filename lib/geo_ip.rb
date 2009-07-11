0ae8c31df5a1d3f2fbdd12dbcab0602e6f35513a
Kurt Gödel <peetldee@gmail.com>
2009-07-11 14:39:31 -0400

    default to no visit passed

--- lib/geo_ip.rb
+++ lib/geo_ip.rb
@@ -15,18 +15,22 @@ module GeoIP
     # ip:: the IP address to be parsed.
     #
     # v:: the object to call attributes on
-    def location(ip, v)
+    def location(ip, v = nil)
       r = db.look_up(ip)
       return v if r.nil?
-      v.ip_latitude = r[:latitude]
-      v.ip_longitude = r[:longitude]
-      v.ip_country_code = r[:country_code]
-      v.ip_country_name = r[:country_name]
-      v.ip_city = r[:city]
-      v.ip_dma_code = r[:dma_code]
-      v.ip_area_code = r[:area_code]
-      v.ip_region = r[:region]
-      v
+      if v
+        v.ip_latitude = r[:latitude]
+        v.ip_longitude = r[:longitude]
+        v.ip_country_code = r[:country_code]
+        v.ip_country_name = r[:country_name]
+        v.ip_city = r[:city]
+        v.ip_dma_code = r[:dma_code]
+        v.ip_area_code = r[:area_code]
+        v.ip_region = r[:region]
+        v
+      else
+        r
+      end
     end
   end
 end
